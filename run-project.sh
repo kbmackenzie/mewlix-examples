a99b74059c2024481a6cd1d6e9641e8a6f186a75
kbmackenzie <kelly.a.betty@gmail.com>
2024-05-23 14:52:50 -0300

    chore(run): Add better logging in run-project.sh script

--- run-project.sh
+++ run-project.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-print_error() {
-  echo "$1" >&2
+SCRIPT_NAME='run-project'
+
+log_message() {
+  echo "[$SCRIPT_NAME] $1"
+}
+
+log_error() {
+  echo "[$SCRIPT_NAME] $1" 1>&2
 }
 
 string_match() {
-  echo "$2" | grep -Ee "$1" > /dev/null 2> /dev/null
+  echo "$2" | grep -Ee "$1" > /dev/null
 }
 
 is_number() {
@@ -13,26 +19,26 @@ is_number() {
 }
 
 if [ -z "$1" ]; then
-  print_error 'No project number specified!'
+  log_error 'No project number specified!'
   exit 1
 fi
 
 if ! is_number "$1"; then
-  print_error "Invalid project number: \"$1\""
+  log_error "Invalid project number: \"$1\""
   exit 1
 fi
 
 PROJECT=$(find . -maxdepth 1 -type d -name "$1*" | head -n 1)
 
 if [ -z "$PROJECT" ]; then
-  print_error "No project found matching number $1!"
+  log_error "No project found matching number $1!"
   exit 1
 fi
 
-echo "Running project $1: \"$PROJECT\""
+log_message "Running project $1: \"$PROJECT\""
 
 cd "$PROJECT" || {
-  print_error "Couldn't cd into project directory \"$PROJECT'\""
+  log_error "Couldn't cd into project directory \"$PROJECT'\""
   exit 1
 }
 mewlix run
